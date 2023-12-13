3a14d2b34ab9b769bf751e4e71eb4050b0aaf539
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:33:25 +0700

    Add schedulerFactoryBean in QuartzConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3quartz/config/QuartzConfig.java
+++ src/main/java/com/hendisantika/springboot3quartz/config/QuartzConfig.java
@@ -1,7 +1,17 @@
 package com.hendisantika.springboot3quartz.config;
 
+import de.chandre.quartz.spring.AutowiringSpringBeanJobFactory;
 import lombok.RequiredArgsConstructor;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +28,26 @@ import org.springframework.context.annotation.Configuration;
 public class QuartzConfig {
 
     private final String QUARTZ_PROPERTIES = "quartz/quartz.properties";
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(
+            final @Autowired ApplicationContext applicationContext)
+            throws SchedulerException, IOException {
+
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        StdSchedulerFactory stdSchedulerFactory = new StdSchedulerFactory(QUARTZ_PROPERTIES);
+
+        SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
+        scheduler.setSchedulerFactory(stdSchedulerFactory);
+        scheduler.setQuartzProperties(this.quartzProperties());
+        scheduler.setConfigLocation(new ClassPathResource(QUARTZ_PROPERTIES));
+        scheduler.setJobFactory(jobFactory);
+        scheduler.setDataSource(this.quartzDataSource());
+        scheduler.setWaitForJobsToCompleteOnShutdown(true);
+        scheduler.setAutoStartup(true);
+
+        return scheduler;
+    }
 }
