1979d18f3c26507c652c29c797301d9e51b438c8
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:35:59 +0700

    Add apiDocConfig in SwaggerConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3quartz/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springboot3quartz/config/SwaggerConfig.java
@@ -1,6 +1,11 @@
 package com.hendisantika.springboot3quartz.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,5 +33,20 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/swagger-ui", baseUrl.concat(REDIRECT_URL));
         registry.addRedirectViewController("/api", baseUrl.concat(REDIRECT_URL));
+    }
+
+    @Bean
+    public OpenAPI apiDocConfig() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Spring Boot 3 Quartz Scheduler")
+                                .description("Spring Boot 3 Quartz Scheduler")
+                                .version("0.0.1")
+                                .contact(new Contact().name("hendisantika").email("hendisantika@yahoo.co.id")))
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("Documentation")
+                                .url("https://t.me/hendisantika34"));
     }
 }
