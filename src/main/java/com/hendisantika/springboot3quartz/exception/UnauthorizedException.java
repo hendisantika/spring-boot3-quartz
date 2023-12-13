2257984961b99fca51f7446c59b38969b8fd001b
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:39:14 +0700

    Add UnauthorizedException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3quartz/exception/UnauthorizedException.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springboot3quartz.exception;
+
+import org.hibernate.service.spi.ServiceException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/13/23
+ * Time: 07:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnauthorizedException extends ServiceException {
+
+    public UnauthorizedException() {
+        super();
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
