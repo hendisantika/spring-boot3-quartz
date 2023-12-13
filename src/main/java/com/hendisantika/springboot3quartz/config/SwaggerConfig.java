39a527efd08d13f96454ca7df64c64e49fe9ba3d
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:35:01 +0700

    Add addViewControllers in SwaggerConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3quartz/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springboot3quartz/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.hendisantika.springboot3quartz.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -17,9 +18,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
-    private static final String REDIRECT_URL = "/swagger-ui.html";
+    private static final String REDIRECT_URL = "/swagger-ui";
 
     @Value("${spring.mvc.servlet.path}")
     private String baseUrl;
 
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", baseUrl.concat(REDIRECT_URL));
+        registry.addRedirectViewController("/swagger-ui", baseUrl.concat(REDIRECT_URL));
+        registry.addRedirectViewController("/api", baseUrl.concat(REDIRECT_URL));
+    }
 }
