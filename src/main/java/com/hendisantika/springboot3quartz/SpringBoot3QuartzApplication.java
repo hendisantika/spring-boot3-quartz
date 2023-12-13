573d11110c75ae7930ef4d435fda7b6cfa29f812
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:25:09 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3quartz/SpringBoot3QuartzApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot3quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3QuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3QuartzApplication.class, args);
+    }
+
+}
