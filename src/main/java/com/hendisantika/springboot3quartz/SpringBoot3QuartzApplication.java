eae875d445c27440c7ef71099c72586dbb8b9923
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:32:10 +0700

    Add modelMapper Bean 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3quartz/SpringBoot3QuartzApplication.java
+++ src/main/java/com/hendisantika/springboot3quartz/SpringBoot3QuartzApplication.java
@@ -1,13 +1,20 @@
 package com.hendisantika.springboot3quartz;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBoot3QuartzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3QuartzApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
