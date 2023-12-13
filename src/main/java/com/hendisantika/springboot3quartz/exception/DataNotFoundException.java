f9797ab764f24414a105f6affe8880c286b0dafc
hendisantika <hendisantika@yahoo.co.id>
2023-12-13 07:37:44 +0700

    Add DataNotFoundException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3quartz/exception/DataNotFoundException.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springboot3quartz.exception;
+
+import org.hibernate.service.spi.ServiceException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/13/23
+ * Time: 07:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataNotFoundException extends ServiceException {
+
+    public DataNotFoundException() {
+        super();
+    }
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
+    public DataNotFoundException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
